363bc99094765301e9ade0dbfb91e4add56d210e
mgear <yura.khlon@gmail.com>
2018-05-20 21:23:42 +0300

    add missed template.xml files

---
+++ extra_addons/digital_memory/models/digital_memory.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+
+from openerp import models, fields, api
+
+class Course(models.Model):
+    _name = 'digital_memory.course'
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
