26a3ce7611bd34b5b852825de1f5fadbfbc694c9
mgear <yura.khlon@gmail.com>
2018-05-23 20:15:07 +0300

    new module

---
+++ extra_addons/digital_memory_ext/__openerp__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Digital Memory",
+
+    'summary': """Manage digital memory""",
+
+    'description': """
+        Digital Memory module for managing wiki:
+            - training courses
+            - training sessions
+            - attendees registration
+    """,
+
+    'author': "Khlon Yuriy",
+    'maintainer': 'Simbioz Holding',
+    'website': "http://www.simbioz.ua",
+
+    # Categories can be used to filter modules in modules listing
+    'category': 'Test',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo.xml',
+        'views/digital_memory.xml',
+    ],
+
+    'installable': True,
+    'auto_install': False,
+
+}
